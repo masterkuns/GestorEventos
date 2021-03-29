e294877f7d5af4c150927480f75e584a884a9f32
yaca <angeldav99@hotmail.com>
2021-03-29 12:00:19 -0500

    modelado

--- model/UserController.php
+++ model/UserController.php
@@ -5,18 +5,15 @@ class UserController
     private $segundoNombre;
     private $primerApellido;
     private $segundoApellido;
-    
+    private $correo;
+    private $contraseña;
+    private $rol;
 
-
-    /**
-     * Get the value of primerNombre
-     */ 
     public function getPrimerNombre()
     {
         return $this->primerNombre;
     }
 
-   
     public function setPrimerNombre($primerNombre)
     {
         $this->primerNombre = $primerNombre;
@@ -24,22 +21,55 @@ class UserController
         return $this;
     }
 
-   
     public function getSegundoNombre()
     {
         return $this->segundoNombre;
     }
 
-    
     public function getPrimerApellido()
     {
         return $this->primerApellido;
     }
 
- 
     public function getSegundoApellido()
     {
         return $this->segundoApellido;
     }
+
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getContraseña()
+    {
+        return $this->contraseña;
+    }
+
+    public function setContraseña($contraseña)
+    {
+        $this->contraseña = $contraseña;
+
+        return $this;
+    }
+
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+
+        return $this;
+    }
+
 }
-?>
